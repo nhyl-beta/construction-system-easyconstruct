70742a6893b04f645f103f01127b960719cc760a
Zeno <nhylcyrusgervasio@gmail.com>
2026-09-18 10:03:14 +0800

    Fixed JWT issues

--- server/src/auth/service.ts
+++ server/src/auth/service.ts
@@ -2,9 +2,9 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import * as repo from "./repository.js";
 import { UnauthorizedError } from "../utils/errors.js";
+import { env } from "../config/env.js";
 import type { LoginInput } from "./types.js";
 
-const JWT_SECRET = process.env.JWT_SECRET ?? "dev-secret-change-me";
 const JWT_EXPIRES_IN = "8h";
 
 export const login = async (input: LoginInput) => {
@@ -16,7 +16,7 @@ export const login = async (input: LoginInput) => {
 
   const token = jwt.sign(
     { sub: user.id, email: user.email, name: user.name, role: user.role },
-    JWT_SECRET,
+    env.JWT_SECRET,
     { expiresIn: JWT_EXPIRES_IN },
   );
 
